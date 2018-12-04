a27b393f49fa12a76a7e64e017a7e7b3b6478977
hermelen <hermelen.peris@gmail.com>
2018-12-04 09:35:39 +0100

    first commit creating components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styles: [`
+    h3 {
+      color: red;
+    }
+  `]
+  // styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'udemy';
+  name = 'Hermelen';
 }
